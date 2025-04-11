8ebfc41d7f473ff73c4829c55362d18ff2bd540a
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-04-11 15:25:48 +0100

    chore: update vite configuration for improved build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import dts from 'vite-plugin-dts';
+
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({
+    insertTypesEntry: true,
+  }),],
   build: {
     lib: {
-      entry: resolve(__dirname, "src/index.tsx"),
+      entry: resolve(__dirname, "src/index.ts"),
       name: "ib-Image-optimizer",
       fileName: (format) => `ib-image-optimizer.${format}.js`,
     },
