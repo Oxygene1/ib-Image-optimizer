5d647f6e554cd149c545b76a6a582b1c0638c782
Abdulganiyu Ibrahim <abdulganiyutechng@gmail.com>
2025-04-21 13:10:21 -0700

    vite declation build congurations added

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      include: ["src"],
+      exclude: ["src/__tests__", "src/**/*.test.ts", "src/**/*.test.tsx"],
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
@@ -22,6 +30,7 @@ export default defineConfig({
     },
     target: "esnext",
     sourcemap: true,
+    outDir: "dist",
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js", ".jsx", ".d.ts"],
