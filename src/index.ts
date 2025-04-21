329bcdbe76a7b3826621cfd7d45f1fa9a0759b64
Abdulganiyu Ibrahim <abdulganiyutechng@gmail.com>
2025-04-21 15:23:37 -0700

    library compatibility with ssr and clients framworks

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export * from "./core/image-optimizer";
-export { ImageOptimizer } from "./use-react";
-export type { ImageOptimizerProps } from "./use-react";
+export { ImageOptimizer, type ImageOptimizerProps } from "./use-react";
+export { Image, type ImageProps } from "./components/ui/image";
