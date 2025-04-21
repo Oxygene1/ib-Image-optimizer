67e7ea00d9e7e595783279bd10ffdf30996e0e98
Abdulganiyu Ibrahim <abdulganiyutechng@gmail.com>
2025-04-21 16:59:10 -0700

    ssr code updated

--- src/use-react/server.tsx
+++ src/use-react/server.tsx
@@ -1,19 +1,37 @@
 import dynamic from "next/dynamic";
 import type { ImageOptimizerProps } from "./client";
+import { Skeleton } from "../components/ui/skeleton";
+import { cn } from "../lib/utils";
 
 const ClientImageOptimizer = dynamic(
   () => import("./client").then((mod) => mod.ClientImageOptimizer),
   {
     ssr: false,
-    loading: () => (
-      <div className="relative aspect-auto">
-        <div className="absolute inset-0 bg-gray-200 animate-pulse" />
-      </div>
-    ),
+    loading: () => <Skeleton className="w-full h-full" />,
   }
 );
 
+const ServerImageOptimizer = ({
+  aspectRatio = "auto",
+  className,
+}: ImageOptimizerProps) => {
+  return (
+    <div
+      className={cn(
+        "relative",
+        aspectRatio && `aspect-${aspectRatio}`,
+        className
+      )}
+    >
+      <Skeleton className="absolute inset-0" />
+    </div>
+  );
+};
+
 export const ImageOptimizer = (props: ImageOptimizerProps) => {
+  if (typeof window === "undefined") {
+    return <ServerImageOptimizer {...props} />;
+  }
   return <ClientImageOptimizer {...props} />;
 };
 
