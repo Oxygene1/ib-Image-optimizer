3d9523824ed8b125b19eb3144a4825e90267dd09
Abdulganiyu Ibrahim <abdulganiyutechng@gmail.com>
2025-05-05 18:08:28 -0700

    files updated

--- src/use-react/index.tsx
+++ src/use-react/index.tsx
@@ -1,8 +1,6 @@
 import { ImageOptimizerProps } from "@/types";
 import ClientImageOptimizer from "./client";
 
-
-
 export const ImageOptimizer = ({ ...props }: ImageOptimizerProps) => {
   return <ClientImageOptimizer {...props} />;
 };
